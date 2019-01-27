553733b7a1471538c6fa84e50248ce6ac4c08c6a
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-01-27 23:50:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 function happyHolidays(string) {
   return ('Happy holidays!');
 }
+
 function happyHolidaysTo(string) {
   return ('Happy holidays, you!');
 }
+
 function happyHolidayTo(string) {
   return ('Happy Independence Day, you!');
 }
+
 function holidayCountdown(string) {
-  return ("It\'s 3 days until Christmas"!);
+  return ("It\'s 3 days until Christmas!");
 }
